9636d2141b0449e94cd083b4abe58319f7c76b06
Slaven Glumac <Slaven.Glumac@avl.com>
2021-01-08 22:09:25 +0100

    Changing the xml title

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ import master
 def main():
     current_dir = path.dirname(path.abspath(__file__))
     fmu_dir = path.join(current_dir, 'FMUs')
-    slaves, connections = configuration.read(fmu_dir, 'FMUsXML.xml')
+    slaves, connections = configuration.read(fmu_dir, 'example.xml')
     results = master.run(slaves, connections, 1e-1, 10.)
     print(results)
 
